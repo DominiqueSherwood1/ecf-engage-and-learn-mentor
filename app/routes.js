1ceb8cf45342e834b0cb5d881e229d93e07ccf1d
Dominique <dominique.sherwood@gmail.com>
2021-04-12 16:13:39 +0100

    branching

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/guidance-question-answer', function (req, res) {
+  const readGuidance = req.session.data['read-guidance']
+
+  if (readGuidance === "yes") {
+    res.redirect('/it3/guidance')
+  } else {
+    res.redirect('/it3/home')
+  }
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
